4c7f56bef54edcba7b8509fbcce9fb6e2a1f85f4
Dillon Good <dilloncgood@gmail.com>
2015-06-12 08:37:19 -0600

    chapter 6

--- 06_join_for_loop/src.js
+++ 06_join_for_loop/src.js
@@ -5,7 +5,47 @@
 module.exports = {
 
   joinWithFor: function(list) {
-    // your code here
-  }
+    result = [];
+    for (i = 0; i < list.length; i++) {
+      result.push(list[i]);
+    }
+    return result.join('');
+  },
 
+  joinWithForAndIndex: function(key, value) {
+    result = [];
+    for (i = 0; i < key.length; i++) {
+      result.push(key[i]);
+      result.push(i);
+    }
+    return result.join('');
+  },
+
+  joinWithoutOddCharacters: function(list) {
+    result = [];
+    for (var i = 0; i < list.length; i++) {
+      if (i % 2 === 0) {
+        result.push(list[i]);
+      }
+    }
+      return result.join('')
+  },
+
+  joinWithForAndToken: function(list) {
+    return list.join(' ').replace(/ /g, "*");
+  },
+
+  joinWithForAndAlternatingTokens: function(list, token1, token2) {
+    var result = [];
+    for (var i = 0; i < list.length; i++) {
+      if (i === list.length - 1) {
+        result.push(list[i])
+    } else if (i % 2 === 0){
+      result.push(list[i] + token1);
+    } else {
+      result.push(list[i] + token2);
+    }
+  }
+  return result.toString().replace(/,/g, "");
+  },
 }
